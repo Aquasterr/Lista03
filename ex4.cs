83728e59bd66ebde0b8488c2bd92378626e20bdf
Aquasterr <110834755+Aquasterr@users.noreply.github.com>
2022-10-03 11:27:37 -0300

    Create ex4.cs

---
+++ ex4.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /* Fazer método que recebe 3 valores e o método retorna o menor dentre eles. */
+        static void Main(string[] args)
+        {
+            double[] v = new double [3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Digite um Valor: ");
+                v[i] = double.Parse(Console.ReadLine());
+            }
+            Array.Sort(v);
+            Console.WriteLine("O Menor numero é: ");
+            Console.WriteLine(v[0]);
+        }
+
+        public static double Fahrenheit(double c)
+        {
+            double fahren = (9 * c + 160) / 5;
+            return fahren;
+        }
+    }
+}
